a4f1e393230a537882f596727531520bee462ba7
ilenortx <ilenortx@163.com>
2018-08-30 08:50:25 +0800

    专致优货商城代码首次上传
    Signed-off-by: ilenortx <ilenortx@163.com>

--- cache/c__webroot_micromail_app_views_wapapp_test.volt.php
+++
@@ -1,9 +0,0 @@
-<div class="mui-content">
-	sdfasdf
-</div>
-
-<script>
-mui.post('../Index/test1?XDEBUG_SESSION_START=ECLIPSE_DBGP&KEY=15266949870761', {}, function(data) {
-	alert(data);
-});
-</script>
