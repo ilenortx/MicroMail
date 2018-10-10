93d532eb4497e749e1d0cab47a8538f3914faa1c
xiaoyemin <1490222347@qq.com>
2018-10-10 19:01:27 +0800

    class 里面字符连接Linux报错

--- app/controllers/timedTask/TimedTask.php
+++ app/controllers/timedTask/TimedTask.php
@@ -1,5 +1,7 @@
 <?php
 
+const TTPATH = APP_PATH.'/controllers/timedTask/config.ini';
+
 /**
  * 定时任务
  * @author xiao
@@ -8,13 +10,12 @@
 class TimedTask extends ControllerBase{
 	
 	//private static $ttpath = APP_PATH.'/controllers/timedTask/config.ini';
-	const TTPATH = APP_PATH.'/controllers/timedTask/config.ini';
 	
 	/**
 	 * 获取所有定时任务
 	 */
 	public static function allTimedTask(){
-		$tts = IniFileOpe::getIniFile(self::TTPATH);
+		$tts = IniFileOpe::getIniFile(TTPATH);
 		
 		if ($tts) return $tts;
 		else return array();
@@ -24,7 +25,7 @@ class TimedTask extends ControllerBase{
 	 * 更新任务信息
 	 */
 	public static function updateTimeTask($datas, $sections=''){
-		return IniFileOpe::reinitFile(self::TTPATH, $datas, $sections);
+		return IniFileOpe::reinitFile(TTPATH, $datas, $sections);
 	}
 	
 	/**
@@ -52,7 +53,7 @@ class TimedTask extends ControllerBase{
 	 * 获取任务信息
 	 */
 	public static function timedTaskInfo($sections){
-		$ttinfo = IniFileOpe::getIniFile(self::TTPATH, $sections);
+		$ttinfo = IniFileOpe::getIniFile(TTPATH, $sections);
 		
 		return $ttinfo;
 	}
